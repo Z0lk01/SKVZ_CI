8544560b958edecd9484f4ace8a49b7e6c1978af
Z0lk01 <milan.zilka1996@gmail.com>
2023-03-28 13:29:06 +0200

    first cloud test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "3zx1g9",
-  "testFiles": "skvz.cy.js", // replace with your test file pattern
-  "video": false, // disable video recording to save disk space
-  "screenshots": false, // disable screenshots to save disk space
+  projectId: "gmp6cf",
+ 
 
   e2e: {
     setupNodeEvents(on, config) {
