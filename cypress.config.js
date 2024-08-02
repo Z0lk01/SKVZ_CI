e2d0fbdb673c40228914132f8e982ec362c69564
Z0lk01 <milan.zilka1996@gmail.com>
2024-08-02 14:02:03 +0200

    some changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  projectId: "gmp6cf",
-  viewportWidth: 1920,
-  viewportHeight: 920,
+module.exports = {
+  projectId: 'gmp6cf',
+  env: {
+    username: "mza",
+    password: "alkoholik",
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
   },
-});
+};
--- cypress/e2e/components.cy.js
+++ cypress/e2e/components.cy.js
@@ -1,27 +1,54 @@
 /// <reference types="cypress" />
 const { type } = require("os");
 describe('Test TSS', () => {
-        beforeEach(() => {
-        cy
-        .clearCookies()
-        cy
-        .clearLocalStorage()
-        cy
-        .visit("https://support.tssmonitoring.sk/login")
-        cy.get('#user_login') 
-        .type("mmza")
-        cy.get('#user_pass')
-        .type("alkoholik")
-        cy.get("#wp-submit") .click()
-        cy.wait(15000)
-        
-         
-        });
-        
-        it("Component 1", () => {
-        cy.get("#main_menu_search_div").should("be.visible")
-    
-        
+    beforeEach(() => {
+        const username = Cypress.env('username') || 'your-username';
+        const password = Cypress.env('password') || 'your-password';
 
-});
+        if (!username || !password) {
+            throw new Error('Environment variables username and password must be set');
+        }
+
+        cy.clearCookies()
+          .clearLocalStorage()
+          .visit("https://support.tssmonitoring.sk/login")
+          .get('#user_login').type(username)
+          .get('#user_pass').type(password)
+          .get("#wp-submit").click()
+          .wait(15000);
+    });
+        
+    it("Component 1", () => {
+        cy.get("#main_menu_search")
+          .should("be.visible")
+          .should("have.attr", "placeholder", "Vyhľadávanie")   
+          .type("Online")
+          .get("#main-menu-wrapper").children().should('have.length', 3)
+          .get(".fa-remove").should("be.visible").should("have.attr", "onclick", "clear_search_bar();")
+          .click()
+          .get("#main-menu-wrapper li.favorites > a ").should("have.text", "Obľúbené").should('have.css', 'background-color', 'rgb(89, 89, 89)')
+          
+          //označenie elementu a použitie funkcie .then()na extrakciu "outerText" a porovnanie s očakávaným textom
+          .get('#li-online-menu').then($el => {
+                const outerText = $el[0].outerText;
+                expect(outerText).to.equal('Online')
+        });
+         cy.get('#li-online-menu')  
+             .click()
+          .get('#gps_map_main').should("be.visible").children().should("have.length", 3)
+          .get("#gps_map_main")
+             .click()
+         .get('#units-online-search')
+         .should("have.attr" , "placeholder", "Hľadať vozidlo / osobu")
+         .type("942DE")
+         
+       
+
+       
+
+          
+            
+
+
+    });
 });
