db78458ee25a73d7de766dc1de02112ff63d788c
Z0lk01 <milan.zilka1996@gmail.com>
2025-04-25 13:46:57 +0200

    edit

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,27 @@
 const { defineConfig } = require("cypress");
 
-module.exports = {
+module.exports = defineConfig({
   projectId: 'xx58n5',
   env: {
     username: "mza",
     password: ".",
     gdpr: "Toto je testovací opis dokumentu, ktorý môže byť veľmi dlhý. \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    
   },
-
   e2e: {
-    baseUrl: 'https://www.tssmonitoring.sk/login',
+    baseUrl: 'https://www.tssmonitoring.sk',
     setupNodeEvents(on, config) {
-    
+      // Add any node event listeners here
     },
-    
   },
-};
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
   reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true,
-  },
+reporterOptions: {
+  reportDir: 'e2e/reports',
+  overwrite: false,
+  html: false,
+  json: true,
+  embeddedScreenshots: true,
+  inlineAssets: true
+},
+screenshotsFolder: 'e2e/screenshots',
+
 });
