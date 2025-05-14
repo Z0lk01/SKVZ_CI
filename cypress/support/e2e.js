1567607b04844c4832f3fe5ff733d74894a0cd2e
Z0lk01 <milan.zilka1996@gmail.com>
2025-05-14 09:14:18 +0200

    asd

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,3 +4,31 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // failing the test
     return false
   })
+
+
+  import addContext from 'mochawesome/addContext'
+
+const titleToFileName = (title) =>
+  title.replace(/[:\/]/g, '')
+
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    let parent = runnable.parent
+    let filename = ''
+    while (parent && parent.title) {
+      filename = `${titleToFileName(
+        parent.title,
+      )} -- ${filename}`
+      parent = parent.parent
+    }
+    filename += `${titleToFileName(
+      test.title,
+    )} (failed).png`
+    addContext(
+      { test },
+      `../screenshots/${Cypress.spec.name}/${filename}`,
+    )
+  }
+  // always add the video
+  addContext({ test }, `../videos/${Cypress.spec.name}.mp4`)
+})
